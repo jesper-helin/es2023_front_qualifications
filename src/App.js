9a0c09cd8a08cd97bf8b3181b25a426cb8d82948
Jesper H <78618836+jesper-helin@users.noreply.github.com>
2022-09-15 14:41:43 +0300

    Artist filter

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import { getConcerts } from "./services/concertsService";
 
 const App = () => {
   const [concerts, setConcerts] = useState([]);
+  const [filteredConcerts, setFilteredConcerts] = useState([]);
+  const [artistFilter, setArtistFilter] = useState("");
 
   useEffect(() => {
     getConcerts().then((res) => {
       setConcerts(res.concerts);
+      setFilteredConcerts(res.concerts);
     });
   }, []);
+
+  useEffect(() => {
+    if (artistFilter !== "") {
+      setFilteredConcerts(
+        concerts.filter((concert) => concert.artist === artistFilter)
+      );
+    } else {
+      setFilteredConcerts(concerts);
+    }
+  }, [artistFilter]);
 
   return (
     <div className="App">
@@ -21,33 +34,52 @@ const App = () => {
         </div>
       </header>
       <main>
-        <Landing concerts={concerts} />
+        <Landing
+          concerts={concerts}
+          filteredConcerts={filteredConcerts}
+          setArtistFilter={setArtistFilter}
+        />
       </main>
     </div>
   );
 };
 
-const Landing = ({ concerts }) => (
+const Landing = ({ concerts, filteredConcerts, setArtistFilter }) => (
   <div>
     <h1>Checkout these amazing concerts in Graz.</h1>
+    <div className="filters">
+      <select
+        className="filter"
+        onChange={(e) => setArtistFilter(e.target.value)}
+      >
+        <option value="">Artist</option>
+        {concerts.map((concert) => (
+          <option value={concert.artist}>{concert.artist}</option>
+        ))}
+      </select>
+    </div>
     <div className="cards">
-      {concerts.map(concert => {
-        return (
-            concert.shows.map((show, key) => (
-              <button className="card" key={key}>
-                <div className="card-top">
-                {`${("0" + new Date(show.start).getDate()).slice(-2)}/${
-                  "0" + (new Date(show.start).getMonth() + 1)
-                }/${new Date(show.start).getFullYear()}`}
-                <h2>{concert.artist}</h2>
-                <h3>{concert.location.name}</h3>
-                </div>
-                <div className="card-bottom">
-                  {`${("0" + new Date(show.start).getUTCHours()).slice(-2)}:${("0" + new Date(show.start).getMinutes()).slice(-2)}`} - {`${("0" + new Date(show.end).getUTCHours()).slice(-2)}:${("0" + new Date(show.end).getMinutes()).slice(-2)}`}
-                </div>
-              </button>
-            ))
-        );
+      {filteredConcerts.map((concert) => {
+        return concert.shows.map((show, key) => (
+          <button className="card" key={key}>
+            <div className="card-top">
+              {`${("0" + new Date(show.start).getDate()).slice(-2)}/${
+                "0" + (new Date(show.start).getMonth() + 1)
+              }/${new Date(show.start).getFullYear()}`}
+              <h2>{concert.artist}</h2>
+              <h3>{concert.location.name}</h3>
+            </div>
+            <div className="card-bottom">
+              {`${("0" + new Date(show.start).getUTCHours()).slice(-2)}:${(
+                "0" + new Date(show.start).getMinutes()
+              ).slice(-2)}`}{" "}
+              -{" "}
+              {`${("0" + new Date(show.end).getUTCHours()).slice(-2)}:${(
+                "0" + new Date(show.end).getMinutes()
+              ).slice(-2)}`}
+            </div>
+          </button>
+        ));
       })}
     </div>
   </div>
